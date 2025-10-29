bbcfc8f1219dae8e8a7d9a89cea993059236f6dd
jh3828-ops <jh3828@gmail.com>
2025-10-29 12:41:46 -0600

    webhook_local.py

--- webhook_local.py
+++ webhook_local.py
@@ -25,4 +25,7 @@ def receive_message():
 
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
